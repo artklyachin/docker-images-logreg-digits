ca66c3fd258c79e358f3db7195694ee7bf5e188a
Artemiy Klyachin <kliachin.av@phystech.su>
2026-04-04 19:16:11 +0300

    first commit

---
+++ checking_image_cpu/check.py
@@ -0,0 +1,8 @@
+import sys, subprocess, tempfile, git
+
+repo_url = sys.argv[1]
+tmp = tempfile.mkdtemp()
+git.Repo.clone_from(repo_url, tmp)
+result = subprocess.run(["python", f"{tmp}/digits_classifier.py"], capture_output=True)
+# парсим вывод, считаем баллы
+print("Score: 5")  # parse_output: true прочитает это
